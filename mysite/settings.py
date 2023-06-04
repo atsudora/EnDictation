3c31362e2624d461743fd22057248b9ed60854cd
atsudora <atsudorasan@gmail.com>
2023-06-04 21:19:49 +0900

    firstcommit

--- mysite/settings.py
+++ mysite/settings.py
@@ -5,7 +5,6 @@ from dj_database_url import parse as dburl  ## 追加
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -46,10 +45,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+# Tepmpateフォルダへの絶対パスを定義
+TEMPLATE_DIR = BASE_DIR / "Template"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,11 +105,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / "static"
+STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 MEDIA_URL = '/media/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('mysite.api.urls')),
     ##path('', include('app.urls')),
