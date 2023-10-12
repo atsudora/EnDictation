f3ed0ceeba99349c7f454e9fd5788767d7e9ae5e
atsudora <atsudorasan@gmail.com>
2023-10-12 22:26:19 +0900

    💄 トップページとランダム問題ページのデザイン微修正

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 export const RANDUM_SENTENCE_URL_API = window.RANDUM_SENTENCE_URL_API;
 export const exclude_pattern = [' ', '?', '!', '\'', '.', ',', '？', '！', '-'];
-export const open_text = '日本語訳を見る <i class="fa-solid fa-toggle-on"></i><br>（Enter）';
-export const close_text = '日本語訳を隠す <i class="fa-solid fa-toggle-off"></i><br>（Enter）';
+export const open_text = '<span class="text-hide">日本語訳を見る</span><i class="fa-solid fa-toggle-on"></i><br><span class="text-hide">（Enter）</span>';
+export const close_text = '<span class="text-hide">日本語訳を隠す</span><i class="fa-solid fa-toggle-off"></i><br><span class="text-hide">（Enter）</span>';
